477f62e9fa24be0a5361d056c4d94d9a314dc74d
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-30 13:34:12 +0200

    Ex 3 MyString final

--- MyString.java
+++ MyString.java
@@ -18,13 +18,32 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        if (str == null) 
+            return null; 
+
+        String result = ""; 
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i); 
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+            result += ch;
+        }
+        return result;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        String newStr1 = lowerCase(str1);
+        String newStr2 = lowerCase(str2);
+
+        if (newStr2.isEmpty())
+            return true;
+            
+        for (int i = 0; i <= newStr1.length() - newStr2.length(); i++) {
+        if (newStr1.substring(i, i + newStr2.length()).equals(newStr2)) 
+            return true;
+        }
+    return false;
     }
 }
